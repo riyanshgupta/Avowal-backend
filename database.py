348b9a5d168bfbec30086f8059f54c264b323dc7
Riyansh Gupta <riyanshgupta@Riyansh-Gupta.local>
2025-09-04 11:08:19 +0530

    🚀 added: pooling added with ssl mode

--- database.py
+++ database.py
@@ -14,13 +14,13 @@ logger = logging.getLogger(__name__)
 engine = create_async_engine(
     DATABASE_URL, 
     echo=False,
-    poolclass=NullPool,
-    # pool_size=20,
-    # max_overflow=30,
-    # pool_timeout=30,
-    # pool_recycle=3600,
-    # pool_pre_ping=True,
-    connect_args={"server_settings": {"jit": "off"}, 'statement_cache_size': 0, 'prepared_statement_name_func': lambda: str(uuid.uuid4())}
+    pool_size=10,  # Adjust based on load
+    max_overflow=20,
+    pool_timeout=30,
+    pool_recycle=3600,
+    pool_pre_ping=True,  # Checks connection health
+    connect_args={"ssl": "require"}
+    # connect_args={"server_settings": {"jit": "off"}, 'statement_cache_size': 0, 'prepared_statement_name_func': lambda: str(uuid.uuid4())}
 )
 # Async session maker
 async_session = async_sessionmaker(
